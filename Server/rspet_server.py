9cfb0c859902c13c0432eaf43d1d23038e360f8c
Dimitris Zervas <dzervas@dzervas.gr>
2016-06-23 03:58:23 +0300

    Added working sockets

--- Server/rspet_server.py
+++ Server/rspet_server.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python2
+from __future__ import print_function
+from sys import exit as sysexit
+from sys import argv
+from thread import start_new_thread
+from threading import Thread
+from socket import socket, AF_INET, SOCK_STREAM
+from socket import error as sock_error
+import tab
+
 class Console:
     """Class to interfere with the server"""
-    def max_conns
-    def server
-    def prompt
+    max_conns = 5 # Maximum connections that the server accepts
+    server = None # The server object
+    prompt = "~$ " # Current command prompt
 
     def __init__(self, max_conns=5):
         """Starts server"""
         self.max_conns = max_conns
-        self.prompt = "~$ "
         self.server = Server()
+
+    def __del__(self):
+        del self.server
 
     def loop(self):
         """Main CLI loop"""
         self._logo()
+        print(self.server.ip)
+        start_new_thread(self.server.loop, ())
+        while True:
+            cmd = raw_input(self.prompt)
+
+            cmdargs = cmd.split(" ")
+            cmd = cmdargs[0]
+            del cmdargs[0]
 
     def _logo(self):
         """Print logo and Authorship/Licence."""
@@ -43,20 +62,39 @@ class Console:
 
 class Server:
     """Main class of the server. Manages all the connected hosts"""
-    def ip = "0.0.0.0"
-    def port = "9000"
-    def hosts = [] # List of hosts
-    def selection = [] # List of selected hosts
-    def plugins = [] # List of active plugins
+    ip = "0.0.0.0"
+    port = "9000"
+    max_conns = 5
+    hosts = [] # List of hosts
+    selection = [] # List of selected hosts
+    plugins = [] # List of active plugins
+    sock = None
 
-    def __init__(self, ip=None, port=None):
+    def __init__(self, max_conns=5, ip="0.0.0.0", port="9000"):
         """Starts to listen on socket"""
+        self.ip = ip
+        self.port = port
+        self.max_conns = max_conns
+
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.setblocking(1)
+        try:
+            self.sock.bind((ip, int(port)))
+            self.sock.listen(max_conns)
+        except sock_error:
+            print("Something went during binding & listening")
 
     def __del__(self):
         """Safely closes all sockets"""
+        for host in self.hosts:
+            del host
+        self.sock.close()
 
     def loop(self):
         """Main server loop. Better call it on its own thread"""
+        while True:
+            (csock, (ip, port)) = self.sock.accept()
+            self.hosts.append(Host(csock, ip, port))
 
     def select(self, ids=None):
         """Selects given host(s) based on ids
@@ -65,8 +103,9 @@ class Server:
         ids     -- Array of ids of hosts. Empty array unselects all. None
         selects all
         """
+        pass
 
-    def exec(self, cmd, args):
+    def execute(self, cmd, args):
         """Execute function on all client objects.
 
         Keyword argument:
@@ -74,6 +113,7 @@ class Server:
         Function signature myfunc(Host, args[0], args[1], ...)
         It should accept len(args) - 1 arguments
         args    -- Arguments to pass to the command function"""
+        pass
 
     def getHost(self, ids=None):
         """Get object(s) of given host(s) based on ids
@@ -81,41 +121,59 @@ class Server:
         Keyword argument:
         ids     -- Array of ids of hosts. None means all.
         """
+        pass
 
     def getSelection(self):
         """Get object(s) of currently selected host(s)"""
+        pass
 
 class Host:
     """Class for host. Each Host object represent one client"""
-    def ip
-    def port
-    def version
-    def type = "full"
-    def sock
+    ip = None
+    port = None
+    version = None
+    type = "full"
+    sock = None
 
-    def __init__(self, ip, port):
+    def __init__(self, sock, ip, port):
         """Accepts the connection and initializes variables"""
+        self.sock = sock
+        self.ip = ip
+        self.port = port
+
+        tmp = self.recv().split("-")
+        self.version = tmp[0]
+        self.type = tmp[1]
+        print(self.ip, self.port, self.version, self.type)
 
     def __del__(self):
         """Graceful deletion of host"""
-
-    def loop(self):
-        """Main loop to manage the host"""
+        self.sock.close()
 
     def send(self, msg):
         """Send message to host"""
+        return self.sock.send(self._enc(msg))
 
-    def recv(self, size):
+    def recv(self, size=1024):
         """Receive from host"""
-
-    def ping(self):
-        """Ping host"""
+        return self._dec(self.sock.recv(size))
+        # return self.sock.recv(size)
 
     def _enc(self, data):
         """Encrypt message (before send)"""
+        out = bytearray(data, 'UTF-8')
+        for i in range(len(out)):
+            out[i] = out[i] ^ 0x41
+
+        return out
 
     def _dec(self, data):
         """Decrypt message (after receive)"""
+        out = bytearray(data)
+        for i in range(len(out)):
+            out[i] = out[i] ^ 0x41
+
+        return out
 
 if __name__ == "__main__":
     cli = Console()
@@ -123,3 +181,6 @@ if __name__ == "__main__":
         cli.loop(int(argv[1]))
     except IndexError:
         cli.loop()
+    except KeyError:
+        del cli
+        sysexit()
