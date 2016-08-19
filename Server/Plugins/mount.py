49c25ac03826c54b0ed0ab506bb40374abca9efe
Kolokotronis Panagiotis <panos122008@gmail.com>
2016-08-19 14:20:34 +0300

    Update mount.py
    Added states handling.
    Split hasattr checks. Now initializing only the required dictionary.

--- Server/Plugins/mount.py
+++ Server/Plugins/mount.py
@@ -9,20 +9,33 @@ class PluginMount(type):
         """Called when a Plugin derived class is imported
 
         Gathers all __host_commands__ to __host_cmds__
-        and all __server_commands__ to __server_cmds__"""
-
-        if not hasattr(cls, "__host_cmds__") or not hasattr(cls, "__server_cmds__"):
-            cls.__server_cmds__ = {}
+        and all __server_commands__ to __server_cmds__
+        Generate __cmd_states__"""
+        if not hasattr(cls, "__cmd_states__"):
+            cls.__cmd_states__ = {}
+        if not hasattr(cls, "__host_cmds__"):
             cls.__host_cmds__ = {}
-        else:
-            tmp = cls()
-            if hasattr(cls, "__host_commands__"):
-                for cmd in tmp.__host_commands__:
-                    cls.__host_cmds__[cmd] = tmp.__host_commands__[cmd]
-            if hasattr(cls, "__server_commands__"):
-                for cmd in tmp.__server_commands__:
-                    cls.__server_cmds__[cmd] = tmp.__server_commands__[cmd]
-            # print("%s was loaded" % name)
+        if not hasattr(cls, "__server_cmds__"):
+            cls.__server_cmds__ = {}
+
+        tmp = cls()
+        if hasattr(cls, "__host_commands__"):
+            for cmd in tmp.__host_commands__:
+                cls.__host_cmds__[cmd] = tmp.__host_commands__[cmd][0]
+                try:
+                    cls.__cmd_states__[cmd] = tmp.__host_commands__[cmd][1:]
+                except:
+                    print("Command %s is not callable from any state" %cmd)
+                    cls.__cmd_states__[cmd] = []
+        if hasattr(cls, "__server_commands__"):
+            for cmd in tmp.__server_commands__:
+                cls.__server_cmds__[cmd] = tmp.__server_commands__[cmd][0]
+                try:
+                    cls.__cmd_states__[cmd] = tmp.__server_commands__[cmd][1:]
+                except:
+                    print("Command %s is not callable from any state" %cmd)
+                    cls.__cmd_states__[cmd] = []
+        #print("%s was loaded" % name)
 
 class Plugin(object):
     """Plugin class (to be extended by plugins)"""
